1bbdb8925920d1eb089327f8f374c2e3f1eae2b3
Majk Tomasz <tmajk@inpost.pl>
2023-02-20 14:19:47 +0100

    zadanko

---
+++ zadanka/zad3.py
@@ -0,0 +1,13 @@
+"""
+Zadanie 3.
+
+Napisz program w ktorym wykonujesz nastepujace kroki:
+1. Wprowadz z klawiatury zakres liczb (od - do)
+2. Wprowadz z klawiatury ile wygenerowac liczb
+3. Wygeneruj tyle liczb ile wprowadzono w pkt2 z zakresu ktory podano w pkt1
+4. Wypisz liczby na ekran
+5. Znajdz w wygenerowanych liczbach z pkt3 liczby pierwsze i wypisz na ekran
+6. Wypisz sume liczb pierwszych na ekran
+
+* mozesz sprobowac napisac funkcje ktore ulatwia ci napisanie programu ;)
+"""
